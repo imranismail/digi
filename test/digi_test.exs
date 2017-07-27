4e4c93519f8d038158184017f80037366ec4859b
Imran Ismail <imran.codely@gmail.com>
2017-07-28 02:48:32 +0800

    Write pseudo tests

--- test/digi_test.exs
+++ test/digi_test.exs
@@ -2,7 +2,88 @@ defmodule DigiTest do
   use ExUnit.Case
   doctest Digi
 
-  test "greets the world" do
-    assert Digi.hello() == :world
+  alias Digi.{Promotion, Checkout, Cashier, Product}
+
+  @ipd_discounted_price 499_99
+
+  setup_all do
+    atv = Product.find("ATV")
+    ipd = Product.find("IPD")
+    vga = Product.find("VGA")
+
+    promo_atv = Promotion.new(name: "Apple TV 3 for 2 Deal",
+                              value: -atv.price,
+                              prerequisite_subtotal_range: {:eq, atv.price * 3},
+                              entitled_skus: ["ATV"],
+                              usage_limit: 1,
+                              allocation_method: :across)
+
+    promo_ipad = Promotion.new(name: "Super iPad Discount",
+                               value: -(ipd.price - @ipd_discounted_price),
+                               prerequisite_subtotal_range: {:gt, ipd.price * 4},
+                               entitled_skus: ["IPD"],
+                               allocation_method: :each)
+
+    promo_mbp = Promotion.new(name: "Free VGA with MBP",
+                              value: -vga.price,
+                              prerequisite_skus: ["VGA"],
+                              entitled_skus: ["MBP"],
+                              allocation_method: :each)
+
+    checkout =
+      Checkout.new()
+      |> Checkout.add_promotion(promo_atv)
+      |> Checkout.add_promotion(promo_ipad)
+      |> Checkout.add_promotion(promo_mbp)
+
+    {:ok, checkout: checkout}
+  end
+
+  test "Counter Scans [ATV, ATV, ATV, VGA]", state do
+    checkout =
+      state[:checkout]
+      |> Cashier.scan("ATV")
+      |> Cashier.scan("ATV")
+      |> Cashier.scan("ATV")
+      |> Cashier.scan("VGA")
+
+    assert Checkout.total(checkout) == 249_00
+  end
+
+  test "Counter Scans [ATV, IPD, IPD, ATV, IPD, IPD, IPD]", state do
+    checkout =
+      state[:checkout]
+      |> Cashier.scan("ATV")
+      |> Cashier.scan("IPD")
+      |> Cashier.scan("IPD")
+      |> Cashier.scan("ATV")
+      |> Cashier.scan("IPD")
+      |> Cashier.scan("IPD")
+      |> Cashier.scan("IPD")
+
+    assert Checkout.total(checkout) == 2718_95
+  end
+
+  test "Counter Scans [MBP, VGA, IPD]", state do
+    checkout =
+      state[:checkout]
+      |> Cashier.scan("MBP")
+      |> Cashier.scan("VGA")
+      |> Cashier.scan("IPD")
+
+    assert Checkout.total(checkout) == 1949_98
+  end
+
+  test "Counter Scans [MBP, VGA, IPD, MBP, VGA, IPD]", state do
+    checkout =
+      state[:checkout]
+      |> Cashier.scan("MBP")
+      |> Cashier.scan("VGA")
+      |> Cashier.scan("IPD")
+      |> Cashier.scan("MBP")
+      |> Cashier.scan("VGA")
+      |> Cashier.scan("IPD")
+
+    assert Checkout.total(checkout) == 1949_98 * 2
   end
 end
